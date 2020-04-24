1d2a4e2615d4161e8fee6516b05cccac8a8a15b7
user1 <user1@DESKTOP-RL1G1AT>
2020-04-24 12:10:43 +0900

    조장

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -2,10 +2,11 @@ package com.kh.git;
 
 public class GitPrac {
 	public static void main(String [] args) {
-		System.out.println("hello~");
-		
+		System.out.println("hello");
+		System.out.println("World");
+		System.out.println("thx");
+		System.out.println("난 조장");
+		// 잘 올라갔겠지?
 	}
 	
 }
-
-
